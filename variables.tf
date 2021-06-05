378744cee4428e1873c6b69cad7e34daa5da611c
binsabbar <ibinsabbar@gmail.com>
2021-06-05 14:30:23 +0300

    Mo: add provider and variables

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "tenancy_ocid" { type = string }
+variable "user_ocid" { type = string }
+variable "fingerprint" { type = string }
+variable "private_key_path" { type = string }
+variable "region" { type = string }
