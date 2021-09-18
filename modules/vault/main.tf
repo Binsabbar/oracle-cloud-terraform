a455517dda05f6b3ed9bbf716dc2046f88145338
binsabbar <ibinsabbar@gmail.com>
2021-09-18 20:23:59 +0300

    Mo: fmt

--- modules/vault/main.tf
+++ modules/vault/main.tf
@@ -2,31 +2,31 @@
 
 resource "oci_kms_vault" "vault" {
   for_each = var.vaults
-  
+
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  vault_type = each.value.type
+  display_name   = each.value.name
+  vault_type     = each.value.type
 }
 
 resource "oci_kms_key" "key" {
-  for_each = { for item in local.flattened_keys: "${item.vault_ref}:${item.key_ref}" => item }
-  
+  for_each = { for item in local.flattened_keys : "${item.vault_ref}:${item.key_ref}" => item }
+
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  desired_state = each.value.enabled ? "ENABLED" : "DISABLED"
+  display_name   = each.value.name
+  desired_state  = each.value.enabled ? "ENABLED" : "DISABLED"
   key_shape {
-      algorithm = each.value.algorithm
-      length = each.value.length
+    algorithm = each.value.algorithm
+    length    = each.value.length
   }
 
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
-  protection_mode = each.value.mode
+  protection_mode     = each.value.mode
 }
 
 resource "oci_kms_key_version" "key_version" {
-  for_each = {for item in local.flattened_keys_rotations: "${item.vault_ref}:${item.key_ref}:${item.version}" => item}
+  for_each = { for item in local.flattened_keys_rotations : "${item.vault_ref}:${item.key_ref}:${item.version}" => item }
 
-  key_id = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
+  key_id              = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
 }
 
@@ -36,34 +36,34 @@ resource "oci_kms_vault" "file_restored_vault" {
   for_each = var.file_restored_vaults
 
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  vault_type = each.value.type
+  display_name   = each.value.name
+  vault_type     = each.value.type
   restore_from_file {
-    content_length = each.value.file.length
-    content_md5 = each.value.file.md5
+    content_length                  = each.value.file.length
+    content_md5                     = each.value.file.md5
     restore_vault_from_file_details = each.value.file.content
   }
 }
 
 resource "oci_kms_key" "key_for_file_restored_vault" {
-  for_each = { for item in local.flattened_keys_for_file_restored_vault: "${item.vault_ref}:${item.key_ref}" => item }
-  
+  for_each = { for item in local.flattened_keys_for_file_restored_vault : "${item.vault_ref}:${item.key_ref}" => item }
+
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  desired_state = each.value.enabled ? "ENABLED" : "DISABLED"
+  display_name   = each.value.name
+  desired_state  = each.value.enabled ? "ENABLED" : "DISABLED"
   key_shape {
-      algorithm = each.value.algorithm
-      length = each.value.length
+    algorithm = each.value.algorithm
+    length    = each.value.length
   }
 
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
-  protection_mode = each.value.mode
+  protection_mode     = each.value.mode
 }
 
 resource "oci_kms_key_version" "key_version_for_file_restored_vault" {
-  for_each = {for item in local.flattened_keys_rotations_for_file_restored_vault: "${item.vault_ref}:${item.key_ref}:${item.version}" => item}
+  for_each = { for item in local.flattened_keys_rotations_for_file_restored_vault : "${item.vault_ref}:${item.key_ref}:${item.version}" => item }
 
-  key_id = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
+  key_id              = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
 }
 
@@ -72,35 +72,35 @@ resource "oci_kms_vault" "object_store_restored_vault" {
   for_each = var.object_store_restored_vaults
 
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  vault_type = each.value.type
+  display_name   = each.value.name
+  vault_type     = each.value.type
   restore_from_object_store {
-    bucket = each.value.oci_object_store.bucket
+    bucket      = each.value.oci_object_store.bucket
     destination = each.value.oci_object_store.destination
-    namespace = each.value.oci_object_store.namespace
-    object = each.value.oci_object_store.object
-    uri = each.value.oci_object_store.uri
+    namespace   = each.value.oci_object_store.namespace
+    object      = each.value.oci_object_store.object
+    uri         = each.value.oci_object_store.uri
   }
 }
 
 resource "oci_kms_key" "key_for_object_restored_vault" {
-  for_each = { for item in local.flattened_keys_for_object_restored_vault: "${item.vault_ref}:${item.key_ref}" => item }
-  
+  for_each = { for item in local.flattened_keys_for_object_restored_vault : "${item.vault_ref}:${item.key_ref}" => item }
+
   compartment_id = each.value.compartment_id
-  display_name = each.value.name
-  desired_state = each.value.enabled ? "ENABLED" : "DISABLED"
+  display_name   = each.value.name
+  desired_state  = each.value.enabled ? "ENABLED" : "DISABLED"
   key_shape {
-      algorithm = each.value.algorithm
-      length = each.value.length
+    algorithm = each.value.algorithm
+    length    = each.value.length
   }
 
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
-  protection_mode = each.value.mode
+  protection_mode     = each.value.mode
 }
 
 resource "oci_kms_key_version" "key_version_for_object_restored_vault" {
-  for_each = {for item in local.flattened_keys_rotations_for_object_restored_vault: "${item.vault_ref}:${item.key_ref}:${item.version}" => item}
+  for_each = { for item in local.flattened_keys_rotations_for_object_restored_vault : "${item.vault_ref}:${item.key_ref}:${item.version}" => item }
 
-  key_id = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
+  key_id              = oci_kms_key.key["${each.value.vault_ref}:${each.value.key_ref}"].id
   management_endpoint = oci_kms_vault.vault[each.value.vault_ref].management_endpoint
 }
--- modules/vault/output.tf
+++ modules/vault/output.tf
@@ -1,59 +1,59 @@
 output "vaults" {
   value = {
-    created_vaults = { for vault_ref, vault in var.vaults: 
+    created_vaults = { for vault_ref, vault in var.vaults :
       vault_ref => {
-        id = oci_kms_vault.vault[vault_ref].id
-        crypto_endpoint = oci_kms_vault.vault[vault_ref].crypto_endpoint
-        management_endpoint =  oci_kms_vault.vault[vault_ref].management_endpoint
-        keys = {for key_ref, key in vault.keys: key_ref => {
-          id = oci_kms_key.key["${vault_ref}:${key_ref}"].id
+        id                  = oci_kms_vault.vault[vault_ref].id
+        crypto_endpoint     = oci_kms_vault.vault[vault_ref].crypto_endpoint
+        management_endpoint = oci_kms_vault.vault[vault_ref].management_endpoint
+        keys = { for key_ref, key in vault.keys : key_ref => {
+          id       = oci_kms_key.key["${vault_ref}:${key_ref}"].id
           vault_id = oci_kms_vault.vault[vault_ref].id
-          versions = {for version in key.versions: 
+          versions = { for version in key.versions :
             version => {
-            id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
-            key_id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
-            is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
+              id         = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
+              key_id     = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
+              is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
             }
           }
-        }}
+        } }
       }
     }
 
-    file_restored_vaults = { for vault_ref, vault in var.file_restored_vaults: 
+    file_restored_vaults = { for vault_ref, vault in var.file_restored_vaults :
       vault_ref => {
-        id = oci_kms_vault.vault[vault_ref].id
-        crypto_endpoint = oci_kms_vault.vault[vault_ref].crypto_endpoint
-        management_endpoint =  oci_kms_vault.vault[vault_ref].management_endpoint
-        keys = {for key_ref, key in vault.keys: key_ref => {
-          id = oci_kms_key.key["${vault_ref}:${key_ref}"].id
+        id                  = oci_kms_vault.vault[vault_ref].id
+        crypto_endpoint     = oci_kms_vault.vault[vault_ref].crypto_endpoint
+        management_endpoint = oci_kms_vault.vault[vault_ref].management_endpoint
+        keys = { for key_ref, key in vault.keys : key_ref => {
+          id       = oci_kms_key.key["${vault_ref}:${key_ref}"].id
           vault_id = oci_kms_vault.vault[vault_ref].id
-          versions = {for version in key.versions: 
+          versions = { for version in key.versions :
             version => {
-            id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
-            key_id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
-            is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
+              id         = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
+              key_id     = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
+              is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
             }
           }
-        }}
+        } }
       }
     }
 
-    object_restored_vaults = { for vault_ref, vault in var.object_store_restored_vaults: 
+    object_restored_vaults = { for vault_ref, vault in var.object_store_restored_vaults :
       vault_ref => {
-        id = oci_kms_vault.vault[vault_ref].id
-        crypto_endpoint = oci_kms_vault.vault[vault_ref].crypto_endpoint
-        management_endpoint =  oci_kms_vault.vault[vault_ref].management_endpoint
-        keys = {for key_ref, key in vault.keys: key_ref => {
-          id = oci_kms_key.key["${vault_ref}:${key_ref}"].id
+        id                  = oci_kms_vault.vault[vault_ref].id
+        crypto_endpoint     = oci_kms_vault.vault[vault_ref].crypto_endpoint
+        management_endpoint = oci_kms_vault.vault[vault_ref].management_endpoint
+        keys = { for key_ref, key in vault.keys : key_ref => {
+          id       = oci_kms_key.key["${vault_ref}:${key_ref}"].id
           vault_id = oci_kms_vault.vault[vault_ref].id
-          versions = {for version in key.versions: 
+          versions = { for version in key.versions :
             version => {
-            id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
-            key_id = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
-            is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
+              id         = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_version_id
+              key_id     = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].key_id
+              is_primary = oci_kms_key_version.key_version["${vault_ref}:${key_ref}:${version}"].is_primary
             }
           }
-        }}
+        } }
       }
     }
   }
