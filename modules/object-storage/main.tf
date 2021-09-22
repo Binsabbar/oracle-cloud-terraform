0ee9c7cb5ec6cff099b9a95be54f303726f5e94a
Abdullah Aljubayri <a.aljubayri@takamol.com.sa>
2021-09-22 18:08:15 +0300

    adding lifecycle policy draft

--- modules/object-storage/main.tf
+++ modules/object-storage/main.tf
@@ -7,9 +7,32 @@ resource "oci_objectstorage_bucket" "bucket" {
   namespace = data.oci_objectstorage_namespace.namespace.namespace
 
   compartment_id        = each.value.compartment_id
-  name                  = each.value.name
+  name                  = each.key
   access_type           = each.value.is_public == true ? "ObjectReadWithoutList" : "NoPublicAccess"
   storage_tier          = each.value.storage_tier
   object_events_enabled = lookup(each.value.optionals, "object_events_enabled", false)
   versioning            = lookup(each.value.optionals, "versioning_enabled", false) ? "Enabled" : "Disabled"
+}
+
+resource "oci_objectstorage_object_lifecycle_policy" "lifecycle_policy" {
+  for_each = var.buckets
+  bucket    = each.key
+  namespace = data.oci_objectstorage_namespace.export_namespace.namespace
+  rules {
+    action     = try(each.value.lifecycle_rules.action, "INFREQUENT_ACCESS")
+    is_enabled = "true"
+    name       = each.value.lifecycle_rules.name
+    ## optional 
+    object_name_filter {
+      exclusion_patterns = [
+      ]
+      inclusion_patterns = [
+      ]
+      inclusion_prefixes = [
+      ]
+    }
+    target      = "objects"
+    time_amount = "each.value.lifecycle_rules.name"
+    time_unit   = "DAYS"
+  }
 }
