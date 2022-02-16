0d607ee1d975324a6539778ce15a4059cf39a244
Mohammed Binsabbar <ibinsabbar@gmail.com>
2022-02-16 16:55:30 +0300

    Mo: adding groups and service_accounts_groups to output

--- modules/identity/output.tf
+++ modules/identity/output.tf
@@ -1,3 +1,11 @@
 output "compartments" {
   value = oci_identity_compartment.compartments
 }
+
+output "groups" {
+  value = oci_identity_group.groups
+}
+
+output "service_accounts_groups" {
+  value = oci_identity_group.service_accounts_groups
+}
