fd1a0f573471e6be6b57fd67ce732ea45bff5779
octopus20 <abeermso11@gmail.com>
2023-01-16 11:40:54 +0300

    AB: Support autoscale k8s cluster with node size=0

--- modules/kubernetes/main.tf
+++ modules/kubernetes/main.tf
@@ -1,3 +1,12 @@
+locals {
+  autoscale_nodes = { for k, v in var.node_pools :
+    k => v if v.size == 0
+  }
+  static_nodes = { for k, v in var.node_pools :
+    k => v if v.size != 0
+  }
+}
+
 resource "oci_containerengine_cluster" "cluster" {
   compartment_id     = var.compartment_id
   name               = var.cluster_name
@@ -19,7 +28,7 @@ resource "oci_containerengine_cluster" "cluster" {
 }
 
 resource "oci_containerengine_node_pool" "node_pool" {
-  for_each = var.node_pools
+  for_each = local.static_nodes
 
   cluster_id         = oci_containerengine_cluster.cluster.id
   compartment_id     = each.value.compartment_id
@@ -43,10 +52,46 @@ resource "oci_containerengine_node_pool" "node_pool" {
       subnet_id           = each.value.subnet_id
     }
   }
-  lifecycle {
-    ignore_changes = [
-      node_config_details.size
-    ]
+
+  node_source_details {
+    image_id                = each.value.image_id
+    source_type             = "IMAGE"
+    boot_volume_size_in_gbs = each.value.volume_size_in_gbs
+  }
+
+  dynamic "node_shape_config" {
+    for_each = length(each.value.flex_shape_config) == 2 ? [1] : []
+    content {
+      memory_in_gbs = each.value.flex_shape_config.memory_in_gbs
+      ocpus         = each.value.flex_shape_config.ocpus
+    }
+  }
+}
+
+resource "oci_containerengine_node_pool" "node_pool_ignored_size" {
+  for_each = local.autoscale_nodes
+
+  cluster_id         = oci_containerengine_cluster.cluster.id
+  compartment_id     = each.value.compartment_id
+  kubernetes_version = each.value.k8s_version
+  name               = "${each.key}-node-pool"
+  node_shape         = each.value.shape
+  ssh_public_key     = each.value.ssh_public_key
+  node_metadata      = each.value.node_metadata
+
+  dynamic "initial_node_labels" {
+    for_each = each.value.labels
+    content {
+      key   = initial_node_labels.key
+      value = initial_node_labels.value
+    }
+  }
+  node_config_details {
+    size = each.value.size
+    placement_configs {
+      availability_domain = each.value.availability_domain
+      subnet_id           = each.value.subnet_id
+    }
   }
 
   node_source_details {
@@ -61,5 +106,11 @@ resource "oci_containerengine_node_pool" "node_pool" {
       memory_in_gbs = each.value.flex_shape_config.memory_in_gbs
       ocpus         = each.value.flex_shape_config.ocpus
     }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      node_config_details[0].size
+    ]
   }
 }
