43f9ebfd8d652386e0abcb124c2134d00745cb24
Remaz <114955324+remaz2250@users.noreply.github.com>
2024-09-05 17:27:41 +0300

    [DT-13799]Remaz: add defined_tags to the node pool resource

--- modules/kubernetes/variables.tf
+++ modules/kubernetes/variables.tf
@@ -47,7 +47,7 @@ variable "node_pools" {
     ssh_public_key      = string
     availability_domain = string
     shape               = string
-    defined_tags        = defined_tags
+    defined_tags        = map(string)
     size                = number
     volume_size_in_gbs  = number
     image_id            = string
