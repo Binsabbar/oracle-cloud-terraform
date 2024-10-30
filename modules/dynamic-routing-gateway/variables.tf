03b1556d624cf6dceadda9336f27fc4ad08f00d9
Mohammed Binsabbar <ibinsabbar@gmail.com>
2024-10-31 01:38:49 +0300

    Mo: init vars

--- modules/dynamic-routing-gateway/variables.tf
+++ modules/dynamic-routing-gateway/variables.tf
@@ -1,5 +1,35 @@
 variable "gateways" {
   type = object({
-    gateways = map(string)
+    gateways = map(object{
+        compartment_id = string
+        name = string
+
+        drg_route_tables = map(object{
+            name = string
+            enable_import_route_distribution = optional(bool, false)
+            enable_ecmp = optional(bool, false)
+
+            rules = map(object{
+                destination_cidr = string
+                next_hop_attachement_type = string
+                next_hop_attachement = string
+            })
+        })
+
+        vcn_attachements = optional(map(object({
+            name = string
+            vcn_compartment_id = string
+            vcn_id = string
+            drg_route_table = 
+        })), {})
+
+        ipsec_attachements = optional(map(object({
+            name = string
+            ipsec_id = string
+        })), {})
+
+        remote_peering_connection = optional(map(any))
+
+    })
   })
 }
