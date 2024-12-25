416fe481267014e8dd8fcb35334263e5a321207c
m.esam <m.esam-c@takamol.sa>
2024-12-25 11:48:35 +0300

    ES: attach all private views to vcn dns resolver

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -19,3 +19,18 @@ output "lpg" {
     gateway_name => value
   }
 }
+
+output "compartments_and_views" {
+  value = {
+    for name, compartment in data.oci_identity_compartments.compartments : name => {
+      compartments = compartment.compartments
+      views = {
+        for compartment_id in [for c in compartment.compartments : c.id] :
+        compartment_id => try(
+          data.oci_dns_views.compartment_views[compartment_id].views,
+          []
+        )
+      }
+    }
+  }
+}
