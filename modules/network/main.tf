e21e8dad5ae19954936ebb11390a62ce6b6cc325
m.esam <m.esam-c@takamol.sa>
2025-01-06 13:52:26 +0300

    ES: update oci_identity_tenancy data source

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -221,8 +221,8 @@ data "oci_identity_tenancy" "tenancy" {
 }
 
 data "oci_identity_compartments" "compartments" {
-  for_each                  = var.tenancy_ocid ? toset(var.attach_views_compartments) : toset([])
-  compartment_id            = var.tenancy_ocid ? data.oci_identity_tenancy.tenancy[0].id : null
+  for_each                  = var.tenancy_ocid != null && var.tenancy_ocid != "" ? toset(var.attach_views_compartments) : toset([])
+  compartment_id            = data.oci_identity_tenancy.tenancy[0].id
   compartment_id_in_subtree = false
   state                     = "ACTIVE"
   name                      = each.value
