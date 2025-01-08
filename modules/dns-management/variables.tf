dd5701222d589706a4bada15f7f10e1e39cee08d
m.esam <m.esam-c@takamol.sa>
2025-01-08 10:59:07 +0300

    ES: update dns-management module

--- modules/dns-management/variables.tf
+++ modules/dns-management/variables.tf
@@ -22,24 +22,19 @@ variable "compartment_id" {
 
 variable "zones" {
   type = map(object({
-    name = string
-  }))
-}
-
-variable "records" {
-  type = map(object({
-    domain_name = string
-    zone_name   = string
-    rtype       = string
-    rdata       = string
-    ttl         = number
+    records = map(object({
+      domain_name = string
+      rdata       = string
+      rtype       = optional(string, "A") # Optional with default value
+      ttl         = optional(number, 300) # Optional with default value
+    }))
   }))
   description = <<EOF
+    Map of DNS zones and their records
+    zone_name - First map key for DNS zones
     domain_name - The target fully-qualified domain name (FQDN) within the target zone.
-    zone_name - The name or OCID of the target zone.
     rtype - The type of the target RRSet within the target zone.
     rdata - The record's data, as whitespace-delimited tokens in type-specific presentation format. All RDATA is normalized and the returned presentation of your RDATA may differ from its initial input. For more information about RDATA, see Supported DNS Resource Record Types
     ttl - The Time To Live for the record, in seconds.    
   EOF
-
 }
