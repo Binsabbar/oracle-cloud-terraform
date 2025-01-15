8aa1c4696b79fc150e7428e4df1d5a7d359bfc3e
m.esam <m.esam-c@takamol.sa>
2025-01-15 17:10:59 +0300

    ES: update network module attach views to dns resolver based on priority number

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -212,20 +212,31 @@ data "oci_core_vcn_dns_resolver_association" "vcn_dns_resolver_association" {
 }
 
 locals {
-  sorted_views = sort([
+  # First create a list of objects from the map
+  views_list = [
     for k, v in var.dns_private_views : {
       key      = k
       view_id  = v.view_id
       priority = v.priority
     }
-  ], "priority") # Just use "priority" instead of [".priority"]
+  ]
+  
+  # Sort the list based on priority
+  sorted_views = sort(local.views_list[*].priority)
+  
+  # Create the final map in priority order
+  views_map = {
+    for priority in local.sorted_views :
+      local.views_list[index(local.views_list[*].priority, priority)].key =>
+      local.views_list[index(local.views_list[*].priority, priority)]
+  }
 }
 
 resource "oci_dns_resolver" "dns_resolver" {
   resolver_id = data.oci_core_vcn_dns_resolver_association.vcn_dns_resolver_association.dns_resolver_id
 
   dynamic "attached_views" {
-    for_each = { for view in local.sorted_views : view.key => view }
+    for_each = local.views_map
     content {
       view_id = attached_views.value.view_id
     }
