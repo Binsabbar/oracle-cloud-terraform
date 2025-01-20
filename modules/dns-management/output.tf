c69cc1412424e557c12b359b9af9dfd354e60d94
m.esam <m.esam-c@takamol.sa>
2025-01-20 10:59:01 +0300

    ES: update dns module output

--- modules/dns-management/output.tf
+++ modules/dns-management/output.tf
@@ -1,70 +1,63 @@
-# Output for Custom Views
-output "custom_views_info" {
-  description = "Basic information about created custom DNS views"
+output "dns_configuration" {
+  description = "Hierarchical structure of DNS views, their zones, and records"
   value = {
-    for key, view in oci_dns_view.custom_view : key => {
-      name           = view.display_name
-      compartment_id = view.compartment_id
-      id             = view.id
-    }
-  }
-}
-
-# Output for Protected Views
-output "protected_views_info" {
-  description = "Basic information about protected DNS views"
-  value = {
-    for key, view in data.oci_dns_view.protected_view : key => {
-      name           = view.display_name
-      compartment_id = view.compartment_id
-      id             = key
-    }
-  }
-}
-
-# Output for Zones
-output "zones_info" {
-  description = "Basic information about created DNS zones"
-  value = {
-    custom_view_zones = {
-      for key, zone in oci_dns_zone.private_dns_zone_custom_view : key => {
-        name           = zone.name
-        compartment_id = zone.compartment_id
-        view_id        = zone.view_id
+    protected_views = {
+      for view_key, view in data.oci_dns_view.protected_view : view_key => {
+        view_details = {
+          name           = view.display_name
+          compartment_id = view.compartment_id
+          id             = view_key
+        }
+        zones = {
+          for zone_key, zone in oci_dns_zone.private_dns_zone_protected_view : zone_key => {
+            if zone.view_id == view_key
+            zone_details = {
+              name           = zone.name
+              compartment_id = zone.compartment_id
+            }
+            records = {
+              for record_key, record in oci_dns_rrset.dns_rrset_protected_view : record_key => {
+                if record.zone_name_or_id == zone.name
+                domain = record.domain
+                rtype  = record.rtype
+                items = [for item in record.items : {
+                  rdata = item.rdata
+                  ttl   = item.ttl
+                }]
+              }
+            }
+          }
+        }
       }
     }
-    protected_view_zones = {
-      for key, zone in oci_dns_zone.private_dns_zone_protected_view : key => {
-        name           = zone.name
-        compartment_id = zone.compartment_id
-        view_id        = zone.view_id
-      }
-    }
-  }
-}
 
-# Output for Records
-output "records_info" {
-  description = "Basic information about created DNS records"
-  value = {
-    custom_view_records = {
-      for key, record in oci_dns_rrset.dns_rrset_custom_view : key => {
-        domain = record.domain
-        rtype  = record.rtype
-        items = [for item in record.items : {
-          rdata = item.rdata
-          ttl   = item.ttl
-        }]
-      }
-    }
-    protected_view_records = {
-      for key, record in oci_dns_rrset.dns_rrset_protected_view : key => {
-        domain = record.domain
-        rtype  = record.rtype
-        items = [for item in record.items : {
-          rdata = item.rdata
-          ttl   = item.ttl
-        }]
+    custom_views = {
+      for view_key, view in oci_dns_view.custom_view : view_key => {
+        view_details = {
+          name           = view.display_name
+          compartment_id = view.compartment_id
+          id             = view.id
+        }
+        zones = {
+          for zone_key, zone in oci_dns_zone.private_dns_zone_custom_view : zone_key => {
+            if zone.view_id == view.id
+            zone_details = {
+              name           = zone.name
+              compartment_id = zone.compartment_id
+            }
+            records = {
+              for record_key, record in oci_dns_rrset.dns_rrset_custom_view : record_key => {
+                if record.zone_name_or_id == zone.name
+                domain = record.domain
+                rtype  = record.rtype
+                items = [for item in record.items : {
+                  rdata = item.rdata
+                  ttl   = item.ttl
+                }]
+              }
+            }
+          }
+        }
       }
     }
   }
