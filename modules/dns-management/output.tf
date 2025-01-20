f0854b49e9339bfa4c0707f53d0a83d15fa06be6
m.esam <m.esam-c@takamol.sa>
2025-01-20 13:36:35 +0300

    ES: update dns module output

--- modules/dns-management/output.tf
+++ modules/dns-management/output.tf
@@ -1,24 +1,25 @@
 output "custom_views_info" {
   value = {
-    for v_key, view in var.private_dns.custom_views : v_key => {
+    for v_key, view in oci_dns_view.custom_view : v_key => {
       view_key = v_key
+      view_name = view.display_name
       zones = {
-        for z_key, zone in view.zones : z_key => {
-          z_key     = z_key
-          zone_name = zone.zone_name
+        for z_key, zone in oci_dns_zone.private_dns_zone_custom_view : z_key => {
+          z_key = z_key
+          zone_name = zone.name
           records = {
-            for r_key, record in zone.records : r_key => {
-              r_key       = r_key
-              domain_name = record.domain_name
-              rdata       = record.rdata
-              rtype       = record.rtype
-              ttl         = record.ttl
-            }
+            for r_key, record in oci_dns_rrset.dns_rrset_custom_view : r_key => {
+              r_key = r_key
+              domain = record.domain
+              rtype = record.rtype
+              items = record.items
+            } if split("-", r_key)[0] == v_key && split("-", r_key)[1] == split("-", z_key)[1]
           }
-        }
+        } if split("-", z_key)[0] == v_key
       }
     }
   }
+  description = "Structured output of DNS configuration with transformed keys"
 }
 # # Output for Protected Views
 # output "protected_views_info" {
