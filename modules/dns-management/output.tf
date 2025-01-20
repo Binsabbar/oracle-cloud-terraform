59b32e2fdedbddfe48ac08f0d1a07ab10084c861
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-01-20 12:46:39 +0300

    Mo: add detailed output

--- modules/dns-management/output.tf
+++ modules/dns-management/output.tf
@@ -2,9 +2,19 @@
 output "custom_views_info" {
   description = "Info about created custom DNS views"
   value = {
-    for v_key, view in oci_dns_view.custom_view : "${v_key}: ${view.id}" => {
-      for z_key, zone in oci_dns_zone.private_dns_zone_custom_view["${v_key}-${z_key}"] : z_key => {
-        for r_key, record in oci_dns_rrset.dns_rrset_custom_view["${v_key}-${z_key}-${r_key}"] : r_key => record
+    for v_key, view in oci_dns_view.custom_view : "${v_key}" => {
+      name           = view.display_name
+      compartment_id = view.compartment_id
+      id             = view.id
+      zones = {
+        for z_key, zone in oci_dns_zone.private_dns_zone_custom_view["${v_key}-${z_key}"] : z_key => {
+          name           = zone.name
+          compartment_id = zone.compartment_id
+          id             = zone.id
+          records = {
+            for r_key, record in oci_dns_rrset.dns_rrset_custom_view["${v_key}-${z_key}-${r_key}"] : r_key => record
+          }
+        }
       }
     }
   }
