85a8662f087ff4f99558255d783b978c2ae94661
m.esam <m.esam-c@takamol.sa>
2025-01-20 15:48:05 +0300

    ES: update dns module output

--- modules/dns-management/output.tf
+++ modules/dns-management/output.tf
@@ -1,31 +1,52 @@
-output "custom_views_info" {
+output "dns_configuration" {
+  description = "Hierarchical structure of DNS views, their zones, and records"
   value = {
-    for v_key, view in oci_dns_view.custom_view : "${view.display_name}" => {
-      name           = view.display_name
-      compartment_id = view.compartment_id
-      id             = view.id
-      zones = {
-        for z_key, zone in oci_dns_zone.private_dns_zone_custom_view : "${zone.name}" => {
-          name           = zone.name
-          compartment_id = zone.compartment_id
-          id             = zone.id
-          records = {
-            for r_key, record in oci_dns_rrset.dns_rrset_custom_view : "${record.domain}" => record if record.zone_name_or_id == zone.id
-          }
-        } if zone.view_id == view.id
+    protected_views = {
+      for view_key, view in data.oci_dns_view.protected_view : "${view.display_name}" => {
+        view_details = {
+          name           = view.display_name
+          compartment_id = view.compartment_id
+          id             = view_key
+        }
+        zones = {
+          for zone_key, zone in oci_dns_zone.private_dns_zone_protected_view : "${zone.name}" => {
+            zone_details = {
+              name           = zone.name
+              compartment_id = zone.compartment_id
+              id             = zone.id
+            }
+            records = {
+              for record_key, record in oci_dns_rrset.dns_rrset_protected_view : "${record.domain}" => {
+                items = record.items
+              } if record.zone_name_or_id == zone.id
+            }
+          } if zone.view_id == view_key
+        }
+      }
+    }
+
+    custom_views = {
+      for view_key, view in oci_dns_view.custom_view : "${view.display_name}" => {
+        view_details = {
+          name           = view.display_name
+          compartment_id = view.compartment_id
+          id             = view.id
+        }
+        zones = {
+          for zone_key, zone in oci_dns_zone.private_dns_zone_custom_view : "${zone.name}" => {
+            zone_details = {
+              name           = zone.name
+              compartment_id = zone.compartment_id
+              id             = zone.id
+            }
+            records = {
+              for record_key, record in oci_dns_rrset.dns_rrset_custom_view : "${record.domain}" => {
+                items = record.items
+              } if record.zone_name_or_id == zone.id
+            }
+          } if zone.view_id == view.id
+        }
       }
     }
   }
-  description = "Structured output of DNS configuration with simplified keys"
 }
-# # Output for Protected Views
-# output "protected_views_info" {
-#   description = "Basic information about protected DNS views"
-#   value = {
-#     for key, view in data.oci_dns_view.protected_view : key => {
-#       name           = view.display_name
-#       compartment_id = view.compartment_id
-#       id             = key
-#     }
-#   }
-# }
