c66e9edc889d30e15cc84526f2289ac3fa2fe379
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-01-20 12:45:20 +0300

    Mo: add detailed output

--- modules/dns-management/output.tf
+++ modules/dns-management/output.tf
@@ -1,71 +1,23 @@
 # Output for Custom Views
 output "custom_views_info" {
-  description = "Basic information about created custom DNS views"
+  description = "Info about created custom DNS views"
   value = {
-    for key, view in oci_dns_view.custom_view : key => {
-      name           = view.display_name
-      compartment_id = view.compartment_id
-      id             = view.id
-    }
-  }
-}
-
-# Output for Protected Views
-output "protected_views_info" {
-  description = "Basic information about protected DNS views"
-  value = {
-    for key, view in data.oci_dns_view.protected_view : key => {
-      name           = view.display_name
-      compartment_id = view.compartment_id
-      id             = key
-    }
-  }
-}
-
-# Output for Zones
-output "zones_info" {
-  description = "Basic information about created DNS zones"
-  value = {
-    custom_view_zones = {
-      for key, zone in oci_dns_zone.private_dns_zone_custom_view : key => {
-        name           = zone.name
-        compartment_id = zone.compartment_id
-        view_id        = zone.view_id
-      }
-    }
-    protected_view_zones = {
-      for key, zone in oci_dns_zone.private_dns_zone_protected_view : key => {
-        name           = zone.name
-        compartment_id = zone.compartment_id
-        view_id        = zone.view_id
+    for v_key, view in oci_dns_view.custom_view : "${v_key}: ${view.id}" => {
+      for z_key, zone in oci_dns_zone.private_dns_zone_custom_view["${v_key}-${z_key}"] : z_key => {
+        for r_key, record in oci_dns_rrset.dns_rrset_custom_view["${v_key}-${z_key}-${r_key}"] : r_key => record
       }
     }
   }
 }
 
-# Output for Records
-output "records_info" {
-  description = "Basic information about created DNS records"
-  value = {
-    custom_view_records = {
-      for key, record in oci_dns_rrset.dns_rrset_custom_view : key => {
-        domain = record.domain
-        rtype  = record.rtype
-        items = [for item in record.items : {
-          rdata = item.rdata
-          ttl   = item.ttl
-        }]
-      }
-    }
-    protected_view_records = {
-      for key, record in oci_dns_rrset.dns_rrset_protected_view : key => {
-        domain = record.domain
-        rtype  = record.rtype
-        items = [for item in record.items : {
-          rdata = item.rdata
-          ttl   = item.ttl
-        }]
-      }
-    }
-  }
-}
+# # Output for Protected Views
+# output "protected_views_info" {
+#   description = "Basic information about protected DNS views"
+#   value = {
+#     for key, view in data.oci_dns_view.protected_view : key => {
+#       name           = view.display_name
+#       compartment_id = view.compartment_id
+#       id             = key
+#     }
+#   }
+# }
