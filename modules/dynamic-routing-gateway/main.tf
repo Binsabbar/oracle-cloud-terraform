1bab793e0df44d19f7f268880aca3f9306e53aa2
Abdullah BinRasheed <a.binrasheed@takamol.com.sa>
2025-02-01 22:52:08 +0300

    AR/BT: Add DRG module

--- modules/dynamic-routing-gateway/main.tf
+++ modules/dynamic-routing-gateway/main.tf
@@ -9,12 +9,12 @@ resource "oci_core_drg" "drg" {
 resource "oci_core_drg_attachment" "drg_attachment" {
   for_each           = var.drg_attachment
   drg_id             = oci_core_drg.drg.id
-  display_name       = var.drg_attachment.name
-  drg_route_table_id = lookup(var.drg_attachment.optionals, "drg_route_table_id", oci_core_drg.drg.default_drg_route_tables.vcn)
+  display_name       = each.value.name
+  drg_route_table_id = lookup(each.value.optionals, "drg_route_table_id", oci_core_drg.drg.default_drg_route_tables.vcn)
   network_details {
-    id             = var.drg_attachment.id
-    type           = var.drg_attachment.network_details_type
-    route_table_id = lookup(var.drg_attachment.optionals, "route_table_id", null)
-    vcn_route_type = lookup(var.drg_attachment.optionals, "vcn_route_type", null)
+    id             = each.value.network_details.id
+    type           = each.value.network_details_type
+    route_table_id = lookup(each.value.optionals, "route_table_id", null)
+    vcn_route_type = lookup(each.value.optionals, "vcn_route_type", null)
   }
 }
