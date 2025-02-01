8173fb44ac55bfa72e097b502423cfaea1a329ab
Abdullah BinRasheed <a.binrasheed@takamol.com.sa>
2025-02-01 22:51:17 +0300

    AR/BT: Add DRG module

--- modules/dynamic-routing-gateway/main.tf
+++ modules/dynamic-routing-gateway/main.tf
@@ -1,25 +1,20 @@
 // DRG
 resource "oci_core_drg" "drg" {
-  compartment_id = var.compartment_id
-  display_name   = var.drg_display_name
+  display_name   = var.drg.name
+  compartment_id = var.drg.compartment_id
 }
 
 
 // ATTACHMENT
 resource "oci_core_drg_attachment" "drg_attachment" {
-  for_each = var.drg_attachment
-
-  drg_id       = oci_core_drg.drg.id
-  display_name = var.drg_attachment.name
-
+  for_each           = var.drg_attachment
+  drg_id             = oci_core_drg.drg.id
+  display_name       = var.drg_attachment.name
   drg_route_table_id = lookup(var.drg_attachment.optionals, "drg_route_table_id", oci_core_drg.drg.default_drg_route_tables.vcn)
   network_details {
-    id   = oci_core_vcn.vcn.id
-    type = var.drg_attachment.network_details_type
-
-    #Optional
-    ids            = var.drg_attachment_network_details_id
-    route_table_id = oci_core_route_table.route_table.id
-    vcn_route_type = var.drg_attachment_network_details_vcn_route_type
+    id             = var.drg_attachment.id
+    type           = var.drg_attachment.network_details_type
+    route_table_id = lookup(var.drg_attachment.optionals, "route_table_id", null)
+    vcn_route_type = lookup(var.drg_attachment.optionals, "vcn_route_type", null)
   }
 }
--- modules/dynamic-routing-gateway/variables.tf
+++ modules/dynamic-routing-gateway/variables.tf
@@ -2,26 +2,16 @@ variable "drg" {
   type = object({
     name           = string
     compartment_id = string
-    drg_route_table = map(object({
-      name                             = string
-      enable_import_route_distribution = optional(bool, false)
-      enable_ecmp                      = optional(bool, false)
-
-      rules = map(object({
-        destination_cidr        = string
-        next_hop_attachement_id = string
-      }))
-    }))
   })
 }
 
 variable "drg_attachment" {
   type = object({
     name = string
-
     network_details = optional(map(object({
       id   = string
       type = string
     })), {})
+    optionals = optional(map(string), {})
   })
 }
