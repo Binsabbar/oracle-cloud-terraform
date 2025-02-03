bb4a51e9ecb30cf10a859cb7181279c00f31308d
Abdullah BinRasheed <a.binrasheed@takamol.com.sa>
2025-02-03 16:47:51 +0300

    AR: Make ID input optional

--- modules/dynamic-routing-gateway/variables.tf
+++ modules/dynamic-routing-gateway/variables.tf
@@ -3,9 +3,17 @@ variable "drg" {
   type = object({
     name = string
     attachments = optional(map(object({
-      id        = string
+      id        = optional(string)
       type      = string
       optionals = optional(map(string), {})
     })), {})
   })
+description = <<EOF
+    object to configure DRG and its attachments
+      name       : name of the DRG
+      attachments: map of objects to attachment
+        id       : The network id for the VCN
+        type     : The type of attachment the VCN
+        optionals: The IPv6 CIDR block for the VCN
+  EOF
 }
