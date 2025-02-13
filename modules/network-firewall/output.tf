e45897788cc94031d7ec67a73c45c161518befeb
Mohammed Binsabbar <ibinsabbar@gmail.com>
2025-02-13 18:08:04 +0300

    Mo: remove un wanted test

--- modules/network-firewall/output.tf
+++ modules/network-firewall/output.tf
@@ -5,8 +5,3 @@ output "firewall" {
 output "policies" {
   value = oci_network_firewall_network_firewall_policy.network_firewall_policy
 }
-
-output "test" {
-  value = data.oci_network_firewall_network_firewall_policy.network_firewall_policy
-
-}
