f1d81ef7befba9545a71a3330fe215d7e7e231d3
f.albahli <f.albahli@takamol.sa>
2025-04-29 13:36:39 +0300

    FB: add freeform tags

--- modules/instances/variables.tf
+++ modules/instances/variables.tf
@@ -64,6 +64,7 @@ variable "instances" {
     state                    = string
     autherized_keys          = string
     ipv6                     = optional(bool, false)
+    freeform_tags            = optional(map(string), {})
     config = object({
       shape             = string
       flex_shape_config = map(string)
