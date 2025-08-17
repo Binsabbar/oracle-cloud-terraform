66989b71de72c066da25271b96209a1f8dc05c32
fa.alduhailan <fa.alduhailan@takamol.sa>
2025-08-17 16:10:00 +0300

    Fares: Add output for the tags

--- modules/identity/output.tf
+++ modules/identity/output.tf
@@ -9,3 +9,11 @@ output "groups" {
 output "service_accounts_groups" {
   value = oci_identity_group.service_accounts_groups
 }
+
+output "tag_keys_fqn" {
+  description = "Map of short key -> fully-qualified key (namespace.key)."
+  value = {
+    for k, t in oci_identity_tag.tags :
+    k => "${var.tag_namespace.name}.${t.name}"
+  }
+}
