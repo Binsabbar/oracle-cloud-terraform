2eaa3bd4754f2f7a665e39bb4ffad12984255347
fa.alduhailan <fa.alduhailan@takamol.sa>
2025-08-20 12:35:12 +0300

    Fares: Add defined_tags definition to the instances module

--- modules/instances/main.tf
+++ modules/instances/main.tf
@@ -46,6 +46,7 @@ resource "oci_core_instance" "instances" {
   preserve_boot_volume = lookup(each.value.optionals, "preserve_boot_volume", true)
   state                = each.value.state
   freeform_tags        = lookup(each.value, "freeform_tags", null)
+  defined_tags         = lookup(each.value, "defined_tags", null)
   metadata = {
     ssh_authorized_keys = each.value.autherized_keys
     user_data           = lookup(each.value.optionals, "user_data", null)
